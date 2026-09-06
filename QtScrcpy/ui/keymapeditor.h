42881d4f859c57d689c3c91ff918cfb173acbe16
gitlid <160818916+gitlid@users.noreply.github.com>
2026-09-06 17:36:46 +0800

    fix: fit keymap snapshot after layout and cover portrait/landscape resizing

--- QtScrcpy/ui/keymapeditor.h
+++ QtScrcpy/ui/keymapeditor.h
@@ -3,18 +3,30 @@
 #include <QDialog>
 #include <QPixmap>
 #include <QPointer>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QTimer>
 #include "keymapdocument.h"
-class QGraphicsScene;class QGraphicsView;class QListWidget;class QLineEdit;class QDoubleSpinBox;class QLabel;
+class QListWidget;class QLineEdit;class QDoubleSpinBox;class QLabel;
 class KeymapEditor : public QDialog {
  Q_OBJECT
 public:
  KeymapEditor(const QPixmap &frame,const QString &script,QWidget *parent=nullptr);
  QString script()const{return QString::fromUtf8(QJsonDocument(m_document.root).toJson());}
  QString savedPath()const{return m_path;}
+protected:
+ void showEvent(QShowEvent *event) override {
+    QDialog::showEvent(event);
+    // Constructor-time widget sizes are not final. Fit only after layout.
+    if(m_view){m_view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);m_view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);}
+    scheduleFit();
+ }
+ void resizeEvent(QResizeEvent *event) override { QDialog::resizeEvent(event);scheduleFit(); }
 private:
+ void scheduleFit(){QTimer::singleShot(0,this,[this](){if(m_view&&m_scene&&!m_scene->sceneRect().isEmpty())m_view->fitInView(m_scene->sceneRect(),Qt::KeepAspectRatio);});}
  void rebuild();void properties();void updateNode();void load();bool save();void apply();void reject()override;
  KeymapDocument m_document;QPixmap m_frame;QString m_path;bool m_dirty=false;bool m_updating=false;
- QGraphicsScene*m_scene;QGraphicsView*m_view;QListWidget*m_list;
- QVector<QLineEdit*>m_keys;QLineEdit*m_switch;QDoubleSpinBox*m_range;QLabel*m_error;
+ QGraphicsScene*m_scene=nullptr;QGraphicsView*m_view=nullptr;QListWidget*m_list=nullptr;
+ QVector<QLineEdit*>m_keys;QLineEdit*m_switch=nullptr;QDoubleSpinBox*m_range=nullptr;QLabel*m_error=nullptr;
 };
 #endif
