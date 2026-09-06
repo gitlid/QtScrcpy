25276b719799b5747015d49f70cb7ef99d92cf92
gitlid <160818916+gitlid@users.noreply.github.com>
2026-09-06 13:12:05 +0800

    feat: define guarded macro dialog lifecycle for RC1

--- QtScrcpy/ui/actionmacrodialog.h
+++ QtScrcpy/ui/actionmacrodialog.h
@@ -3,18 +3,23 @@
 
 #include <QDialog>
 #include <QPointer>
-
 #include "../QtScrcpyCore/include/QtScrcpyCore.h"
 
 class QLabel;
 class QPushButton;
 class QSpinBox;
+class QCloseEvent;
 
 class ActionMacroDialog : public QDialog
 {
     Q_OBJECT
 public:
     explicit ActionMacroDialog(const QString &serial, QWidget *parent = Q_NULLPTR);
+    ~ActionMacroDialog() override;
+    void reject() override;
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
 
 private slots:
     void toggleRecording();
@@ -29,11 +34,18 @@ private slots:
 private:
     QPointer<qsc::IDevice> device() const;
     void refreshFromDevice();
+    bool confirmDiscard();
+    void markDisconnected();
 
     QString m_serial;
     QString m_currentFile;
+    QString m_lastError;
+    QPointer<qsc::IDevice> m_device;
+    bool m_connected = false;
+    bool m_dirty = false;
     QLabel *m_statusLabel = Q_NULLPTR;
     QLabel *m_progressLabel = Q_NULLPTR;
+    QLabel *m_errorLabel = Q_NULLPTR;
     QPushButton *m_recordButton = Q_NULLPTR;
     QPushButton *m_saveButton = Q_NULLPTR;
     QPushButton *m_loadButton = Q_NULLPTR;
@@ -42,5 +54,4 @@ private:
     QSpinBox *m_repeatSpin = Q_NULLPTR;
     QSpinBox *m_intervalSpin = Q_NULLPTR;
 };
-
-#endif // ACTIONMACRODIALOG_H
+#endif
